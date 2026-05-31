cfac02dec9bffdc4b29bff658d0567d960aa2de0
Andrés García <dev@tech-andgar.me>
2026-05-31 15:30:53 -0500

    refactor: move validation to core/validation, update core barrel exports

--- src/core/image/index.ts
+++ src/core/image/index.ts
@@ -1,8 +1,1 @@
 export { MAX_CANVAS_PIXELS, MAX_IMAGE_PIXELS, clampDimensions } from "./canvas";
-export {
-	IMAGE_SIGNATURES,
-	PDF_MAGIC,
-	hasValidSignature,
-	hasPdfMagicBytes,
-	type MagicSignature,
-} from "./validation";
--- src/core/index.ts
+++ src/core/index.ts
@@ -7,15 +7,26 @@ export {
 	LOG_FLUSH_DEBOUNCE_MS,
 } from "./logger";
 
+export { MAX_CANVAS_PIXELS, MAX_IMAGE_PIXELS, clampDimensions } from "./image";
+
 export {
-	MAX_CANVAS_PIXELS,
-	MAX_IMAGE_PIXELS,
-	clampDimensions,
 	IMAGE_SIGNATURES,
 	PDF_MAGIC,
 	hasValidSignature,
 	hasPdfMagicBytes,
 	type MagicSignature,
-} from "./image";
+} from "./validation";
 
 export { type StorageAdapter, localStorageAdapter } from "./storage";
+
+export {
+	analytics,
+	detectUserProperties,
+	ESSENTIAL_CATEGORIES,
+	type AnalyticsEvent,
+	type AnalyticsProvider,
+	type AnalyticsConfig,
+	type ProviderFactory,
+	type UserProperties,
+	type ConsentLevel,
+} from "./analytics";
