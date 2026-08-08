0d31510053f156d18d88eb596040a908296cdb74
Andrés García <dev@tech-andgar.me>
2026-08-07 22:52:42 -0500

    refactor: remove Firefox WebAssembly warning banner component from MainLayout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,5 @@
 import { useTheme } from '../../hooks/theme/useTheme';
 import { CookieConsent } from '../privacy/CookieConsent';
-import { FirefoxWasmBanner } from './FirefoxWasmBanner';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { PwaRenameBanner } from './PwaRenameBanner';
@@ -14,7 +13,6 @@ export function MainLayout({ children }: Readonly<MainLayoutProps>) {
   return (
     <div className="min-h-screen w-full bg-background flex flex-col">
       <PwaRenameBanner />
-      <FirefoxWasmBanner />
       <Header />
       <main className="flex-1">
         <div className="max-w-2xl w-full mx-auto px-4 py-6 sm:py-8">
